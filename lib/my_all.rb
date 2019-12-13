73e1f01563703ffef47006e221a74654b235133b
Arthur White <mister.arthur.white@gmail.com>
2019-12-13 18:49:32 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,6 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
-  
-
+    i = 0
+    block_return_values = []
+    while (i<collection.length)
+        block_return_values << yield(collection[1])
+        i +=1
+    end
+    if block_return_values.include?(false) 
+        return false
+    end
+    return true
 end
+
+my_all([1,2,3]) { 
+    |i| 
+    i<2
+}
